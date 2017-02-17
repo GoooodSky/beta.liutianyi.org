f0fa3495790560cbe297fb162d10053e4dec6817
uchihaty <liutianyi@Liutianyis-MacBook-Pro.local>
2017-02-18 00:08:11 +0800

    Beta 0.0.8
    Add backToTop.
    JS 0.0.1 done.

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@
 
 var sidebar = $('#sidebar'),
 	mask = $('.mask'),
+	backButton = $('.back-to-top'),
 	sidebar_trigger = $('#sidebar_trigger');
 
 function showSidebar() {
@@ -19,8 +20,28 @@ function hideSidebar() {
 	sidebar.css('right', -sidebar.width());
 }
 
+function backToTop() {
+	$('html , body').animate({scrollTop : 0}, 800)
+
+}
+
+
+
+$(window).on('scroll', function () {
+	if ($(window).scrollTop() > $(window).height())
+		backButton.fadeIn();
+	else
+		backButton.fadeOut();
+
+
+})
+
+$(window).trigger('scroll');
+		
+
 sidebar_trigger.on('click', showSidebar)   // showSidebar后不能加()否则会默认执行
 mask.on('click', hideSidebar)
+backButton.on('click', backToTop)
 
 
 
