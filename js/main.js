dbd9e29b85c234e32440065e81f86f8df7864921
uchihaty <liu_tianyi@outlook.com>
2017-06-05 18:05:05 +0800

    Beta 0.6.2

--- js/main.js
+++ js/main.js
@@ -1,52 +1,44 @@
 $(function () {
 
-
-
-
 'use strict';
-var prevTop = 0;
-var currTop = 0;
-
 
 var sidebar = $('#sidebar'),
     clork = $('#clork'),
     date = $('#date'),
 	sidebarOut = $('#sidebar .fa'),
 	mask = $('.mask'),
-	backButton = $('.back-to-top'),
+	backButton = $('#back_top'),
 	sidebar_trigger = $('#sidebar_trigger'),
-	more = $('.more')
-
+	more = $('.more'),
+    nav = $('nav')
 	;
 
 function showSidebar() {
 	mask.fadeIn();
-	sidebar.css('right', 0);
+	sidebar.css({
+		left: 0,
+		top: nav.height()
+	});
 }
 
 function hideSidebar() {
 	mask.fadeOut();
-	sidebar.css('right', -sidebar.width());
+	sidebar.css('left', -sidebar.width());
 }
 
 function backToTop() {
 	$('html , body').animate({scrollTop : 0}, 800)
-
 }
 
 function goToNext() {
-	$('html , body').animate({scrollTop : 800}, 800)
-
+	$('html , body').animate({scrollTop : 700}, 800)
 }
 
 function clorkChange() {
-
 	clork.hide();
 	date.show();
 	date.css('position', 'relative');
-
 }
-
 
 function dateChange() {
 	clork.show();
@@ -54,43 +46,14 @@ function dateChange() {
 	// date.css('position', 'relative');
 }
 
-
-
-
 $(window).on('scroll', function () {
-	if ($(window).scrollTop() > $(window).height()){
-		// backButton.fadeIn();
-		sidebar_trigger.addClass('sidebar_trigger');
-
-		currTop = $(window).scrollTop();
-
-		 if(currTop > prevTop){  //判断是往上还是往下滚动
-			  // sidebar_trigger.fadeOut(1000);
-	  		  backButton.fadeOut(1000);
-
-		 } 
-
-		 else{
-			 sidebar_trigger.fadeIn(200);
-			 backButton.fadeIn(200);
-		 }
-
-		setTimeout(function(){prevTop = currTop},0);
-
-	}
-
-	else{
-		backButton.fadeOut(1000);
-		sidebar_trigger.removeClass('sidebar_trigger');
-	}
-
-
+	if ($(window).scrollTop() > $(window).height())
+		backButton.fadeIn();
+	else
+		backButton.fadeOut();
 })
 
-
-
 $(window).trigger('scroll');
-		
 
 sidebar_trigger.on('click', showSidebar)   // showSidebar后不能加()否则会默认执行
 mask.on('click', hideSidebar)
@@ -100,10 +63,6 @@ backButton.on('click', backToTop)
 more.on('click', goToNext)
 clork.on('mouseover click', clorkChange)
 date.on('mouseout click', dateChange)
-
-
-
-
 
 
 
